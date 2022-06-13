8e3267bc3d5d04c47ee2b62296849e248e734e24
Alex <sobolev.shurik@mail.ru>
2022-06-14 00:33:11 +0300

    Added new controller and pages register and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view("auth.login");
+    }
+
+    public function showRegisterForm()
+    {
+        return view("auth.register");
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            "name" => ["required", "string",],
+            "email" => ["required", "email", "string", "unique:users,email"],
+            "password" => ["required", "confirmed"]
+        ]);
+
+        $user = \App\Models\User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => bcrypt($data["password"])
+        ]);
+
+        if ($user) {
+            auth("web")->login($user);
+        }
+        return redirect(route("home"));
+    }
+}
